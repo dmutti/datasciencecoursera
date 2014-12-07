65c5aa62f239c95248733febef5689eb47da3ac6
Danilo Mutti <dmutti@gmail.com>
2014-12-07 21:43:52 -0200

    Update quiz-week2.r

--- getting-cleaning-data/quiz-week2.r
+++ getting-cleaning-data/quiz-week2.r
@@ -1,2 +1,3 @@
 #numeros negativos indicam que essa quantidade de colunas deve ser ignorada
+#fonte: https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for
 x <- read.fwf("./getdata-wksst8110.for", skip=4, widths = c(-1,9,-5,4,4,-5,4,4,-5,4,4,-5,4,4))
