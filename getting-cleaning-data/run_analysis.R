b2fdd61541c2ddff614a95f5559c07e086ffeb50
Danilo Mutti <dmutti@gmail.com>
2014-12-10 13:00:49 -0200

    Update run_analysis.R

--- getting-cleaning-data/run_analysis.R
+++ getting-cleaning-data/run_analysis.R
@@ -1,46 +1,50 @@
+#http://stackoverflow.com/questions/24715894/faster-way-to-read-fixed-width-files-in-r
 run_analysis <- function(directory) {
-    features = read.table(paste(directory, "/", "/features.txt", sep=""), header=FALSE, stringsAsFactors = FALSE)
+    prepare    
+    features = read.table(paste(directory, "/", "features.txt", sep=""), header=FALSE, stringsAsFactors=FALSE, col.names=c("index", "name"))
     mean_cols = find_mean_columns(features)
     std_dev_cols = find_std_dev_columns(features)
-
-    train_data = get_train_data(directory, mean_cols, std_dev_cols)
-    test_data = get_test_data(directory, mean_cols, std_dev_cols)
-
+    
+    train_data = get_train_data(directory, features, c(mean_cols$index, std_dev_cols$index))
+    test_data = get_test_data(directory, features, c(mean_cols$index, std_dev_cols$index))
+    
     complete_data = rbind(train_data, test_data)
     complete_data
 }
 
+prepare <- function {
+    if (length(grep("LaF", installed.packages(), fixed=TRUE)) == 0) {
+        install.packages("LaF")
+    }
+    library(LaF)
+}
+
 find_mean_columns <- function(features) {
-    rows = grep("mean()", features$V2)
+    rows = grep("mean()", features$name)
     features[rows,]
 }
 
 find_std_dev_columns <- function(features) {
-    rows = grep("std()", features$V2)
+    rows = grep("std()", features$name)
     features[rows,]
 }
 
-get_train_data <- function(directory, mean_cols, std_dev_cols) {
-    get_data(directory, "/train/subject_train_head.txt", "/train/X_train_head.txt", mean_cols, std_dev_cols)
+get_train_data <- function(directory, features, extract_features) {
+    get_data(directory, "train/subject_train.txt", "train/X_train.txt", features, extract_features)
 }
 
-get_test_data <- function(directory, mean_cols, std_dev_cols) {
-    get_data(directory, "/test/subject_test_head.txt", "/test/X_test_head.txt", mean_cols, std_dev_cols)
+get_test_data <- function(directory, features, extract_features) {
+    get_data(directory, "test/subject_test_head.txt", "test/X_test_head.txt", features, extract_features)
 }
 
-get_data <- function(directory, subject_file, data_file, mean_cols, std_dev_cols) {
-    subjects = read.table(paste(directory, "/", subject_file, sep = ""), header=FALSE)
-    data = read.fwf(paste(directory, "/", data_file, sep = ""), header=FALSE, widths = rep(16, 561))
-    complete_data = filter_data(mean_cols, std_dev_cols, subjects, data)
-    colnames(complete_data) = column_names(mean_cols, std_dev_cols)
+get_data <- function(directory, subject_file, data_file, features, extract_features) {
+    subjects = read.table(paste(directory, "/", subject_file, sep = ""), header=FALSE, col.names=c("subject"))
+    data = read.fwf(paste(directory, "/", data_file, sep = ""), header=FALSE, widths=rep(16, 561), col.names=features$name)
+    complete_data = filter_data(extract_features, subjects, data)
+    complete_data = cbind(subjects, complete_data)
     complete_data
 }
 
-filter_data <- function(mean_cols, std_dev_cols, subjects, data) {
-    filtered = data[c(mean_cols$V1, std_dev_cols$V1)]
-    cbind(subjects, filtered)
-}
-
-column_names <- function(mean_cols, std_dev_cols) {
-    c("subject", mean_cols$V2, std_dev_cols$V2)
+filter_data <- function(extract_features, subjects, data) {
+    data[extract_features]
 }
