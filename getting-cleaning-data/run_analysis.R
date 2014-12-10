f8cd193cfebf4b75234fc862ddb96e953953e372
Danilo Mutti <dmutti@gmail.com>
2014-12-10 15:34:24 -0200

    Update run_analysis.R

--- getting-cleaning-data/run_analysis.R
+++ getting-cleaning-data/run_analysis.R
@@ -1,31 +1,35 @@
 #http://stackoverflow.com/questions/24715894/faster-way-to-read-fixed-width-files-in-r
 run_analysis <- function(directory) {
-    prepare    
+    prepare
     features = read.table(paste(directory, "/", "features.txt", sep=""), header=FALSE, stringsAsFactors=FALSE, col.names=c("index", "name"))
     mean_cols = find_mean_columns(features)
     std_dev_cols = find_std_dev_columns(features)
-    
-    train_data = get_train_data(directory, features, c(mean_cols$index, std_dev_cols$index))
-    test_data = get_test_data(directory, features, c(mean_cols$index, std_dev_cols$index))
-    
+    extract_features = c(mean_cols$index, std_dev_cols$index)
+
+    train_data = get_train_data(directory, features, extract_features)
+    test_data = get_test_data(directory, features, extract_features)
+
     complete_data = rbind(train_data, test_data)
-    complete_data
 }
 
-prepare <- function {
+prepare <- function() {
     if (length(grep("LaF", installed.packages(), fixed=TRUE)) == 0) {
         install.packages("LaF")
     }
+    if (length(grep("data.table", installed.packages(), fixed=TRUE)) == 0) {
+        install.packages("data.table")
+    }
     library(LaF)
+    library(data.table)
 }
 
 find_mean_columns <- function(features) {
-    rows = grep("mean()", features$name)
+    rows = grep(".*mean\\(\\).*", features$name)
     features[rows,]
 }
 
 find_std_dev_columns <- function(features) {
-    rows = grep("std()", features$name)
+    rows = grep(".*std\\(\\).*", features$name)
     features[rows,]
 }
 
@@ -34,12 +38,13 @@ get_train_data <- function(directory, features, extract_features) {
 }
 
 get_test_data <- function(directory, features, extract_features) {
-    get_data(directory, "test/subject_test_head.txt", "test/X_test_head.txt", features, extract_features)
+    get_data(directory, "test/subject_test.txt", "test/X_test.txt", features, extract_features)
 }
 
 get_data <- function(directory, subject_file, data_file, features, extract_features) {
-    subjects = read.table(paste(directory, "/", subject_file, sep = ""), header=FALSE, col.names=c("subject"))
-    data = read.fwf(paste(directory, "/", data_file, sep = ""), header=FALSE, widths=rep(16, 561), col.names=features$name)
+    subjects = read.table(paste(directory, "/", subject_file, sep=""), header=FALSE, col.names=c("subject"))
+    data = laf_open_fwf(paste(directory, "/", data_file, sep=""), column_widths=rep(16, 561), column_types=rep("numeric", 561), column_names=sub("\\(\\)", "", features$name))
+    data = data[,]
     complete_data = filter_data(extract_features, subjects, data)
     complete_data = cbind(subjects, complete_data)
     complete_data
