1779eedab02bf047769950d887253541b5f25a54
Danilo Mutti <dmutti@gmail.com>
2015-01-09 14:01:03 -0200

    Update plot1.R

--- exploratory-data-analysis/plot1.R
+++ exploratory-data-analysis/plot1.R
@@ -1,1 +1,8 @@
-#blah
+plot1 <- function() {
+    first_line = read.csv(file = "./household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";", nrows=1)
+    hhpc = read.csv(file = "./household_power_consumption.txt", header = FALSE, stringsAsFactors = FALSE, na.strings="?", sep = ";", skip = 66637, nrows = 2880, col.names = tolower(gsub("_", "", names(first_line))))
+    png("./plot1.png", width = 480, height = 480, units = "px")
+    par(bg = "transparent")
+    hist(hhpc$globalactivepower, col = "orangered", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+    dev.off()
+}
