ec321c86f163979e087bdeb38142afd309f37493
Danilo Mutti <dmutti@gmail.com>
2015-01-09 22:00:08 -0200

    code

--- exploratory-data-analysis/plot1.R
+++ exploratory-data-analysis/plot1.R
@@ -1,3 +1,8 @@
+# this script does the following
+# 1. reads the "Individual household electric power consumption Data Set" from the file [./household_power_consumption.txt]
+# 2. extracts the data regarding household energy usage over a 2-day period in February, 2007 (02/01/2007 and 02/02/2007)
+# 3. constructs a plot
+# 4. saves it to "./plot1.png" with a width of 480 pixels and a height of 480 pixels
 plot1 <- function() {
     first_line = read.csv(file = "./household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";", nrows=1)
     hhpc = read.csv(file = "./household_power_consumption.txt", header = FALSE, stringsAsFactors = FALSE, na.strings="?", sep = ";", skip = 66637, nrows = 2880, col.names = tolower(gsub("_", "", names(first_line))))
