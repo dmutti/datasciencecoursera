27c636ff8019fadbd5db85665b272afffe4ca3d1
Danilo Mutti <dmutti@gmail.com>
2015-01-13 21:39:44 -0200

    new plots and code

--- exploratory-data-analysis/assignment2-plot5.R
+++ exploratory-data-analysis/assignment2-plot5.R
@@ -1,17 +1,17 @@
-plot4 <- function() {
+plot5 <- function() {
     setup()
     SCC <- readRDS("./Source_Classification_Code.rds")
-    SCC <- mutate(SCC, coal = grepl("coal", tolower(as.character(Short.Name)), ignore.case = TRUE))
+    SCC <- mutate(SCC, motorvehicle = grepl("vehicle", tolower(as.character(EI.Sector)), ignore.case = TRUE))
 
     data <- merge(SCC, NEI, by.x="SCC", by.y="SCC")
 
-    filtered <- filter(data, coal == TRUE)
+    filtered <- filter(data, motorvehicle == TRUE & fips == "24510")
     by_year <- group_by(select(filtered, Emissions, year), year)
 
-    png("./assignment2-plot4.png", width = 480, height = 480, units = "px")
+    png("./assignment2-plot5.png", width = 480, height = 480, units = "px")
     par(bg = "transparent")
     options("scipen" = 20)
-    plot(summarize(by_year, sum(Emissions)), ylab = "Total Emissions From Coal Combustion-Related Sources (tons)", xlab = "Year", type = "l", col="red", main = "United States")
+    plot(summarize(by_year, sum(Emissions)), ylab = "Total Emissions from Motor Vehicle Sources (tons)", xlab = "Year", type = "l", col="red", main = "Baltimore City, Maryland")
     dev.off()
 }
 
