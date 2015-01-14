72a140896e7ec6baf310e8435f9c690bc233c52c
Danilo Mutti <dmutti@gmail.com>
2015-01-14 17:17:35 -0200

    more code

---
+++ exploratory-data-analysis/assignment2-plot6.R
@@ -0,0 +1,35 @@
+plot6 <- function() {
+    setup()
+    SCC <- readRDS("./Source_Classification_Code.rds")
+    SCC <- mutate(SCC, motorvehicle = grepl("vehicle", tolower(as.character(EI.Sector)), ignore.case = TRUE))
+
+    data <- merge(SCC, NEI, by.x="SCC", by.y="SCC")
+
+    sum_motor_balt <- summarize_by_fips(data, "24510")
+    sum_motor_lac <- summarize_by_fips(data, "06037")
+
+    png("./assignment2-plot6.png", width = 480, height = 480, units = "px")
+    par(bg = "transparent")
+    options("scipen" = 20)
+    plot(x = c(sum_motor_balt[[1]], sum_motor_lac[[1]]), y = (c(sum_motor_balt[[2]], sum_motor_lac[[2]]) * 1.1), ylab = "Total Emissions from Motor Vehicle Sources (tons)", xlab = "Year", type = "n", main = "Baltimore City x Los Angeles County")
+    lines(sum_motor_balt, col = "blue")
+    lines(sum_motor_lac, col = "red")
+    legend("topright", c("Los Angeles County", "Baltimore City"), lty=1, col=c("red", "blue"), cex=.75)
+    dev.off()
+}
+
+setup <- function() {
+    if (!exists("NEI", envir = .GlobalEnv)) {
+        assign("NEI", readRDS("./summarySCC_PM25.rds"), envir = .GlobalEnv)
+    }
+    if (length(grep("dplyr", installed.packages(), fixed=TRUE)) == 0) {
+        install.packages("dplyr")
+    }
+    library(dplyr)
+}
+
+summarize_by_fips <- function(full_data, code){
+    filtered <- filter(full_data, motorvehicle == TRUE & fips == code)
+    grouped <- group_by(select(filtered, Emissions, year), year)
+    summarize(grouped_motor, sum(Emissions))
+}
